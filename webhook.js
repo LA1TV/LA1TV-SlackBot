3859a5c00ffd821edfa02a6ee9f2721aa3ed2e15
joshhodgson <josh.pbh@gmail.com>
2016-04-11 20:40:57 +0100

    Make webhook into module

--- webhook.js
+++ webhook.js
@@ -1,40 +1,52 @@
 var http = require('http');
 var config = require('./config');
-var server = http.createServer(handleRequest);
-server.listen(config.hookport);
+var events = require('events').EventEmitter;
 
-function handleRequest(req, res) {
-var fullBody = ""
-  req.on('data', function(chunk) {
-    // append the current chunk of data to the fullBody variable
-    fullBody += chunk.toString();
-  });
-  //  var decodedBody = querystring.parse(fullBody);
-  req.on('end', function() {
-    if (fullBody.length > 5) {
-      parser(JSON.parse(fullBody));
+
+function webhook() {
+  events.EventEmitter.call(this);
+  var server = http.createServer(handleRequest);
+  server.listen(config.hookport);
+
+  function handleRequest(req, res) {
+    var fullBody = ""
+    req.on('data', function(chunk) {
+      // append the current chunk of data to the fullBody variable
+      fullBody += chunk.toString();
+    });
+    //  var decodedBody = querystring.parse(fullBody);
+    req.on('end', function() {
+      if (fullBody.length > 5) {
+        parser(JSON.parse(fullBody));
+      }
+      res.end("Gotcha");
+    });
+
+  }
+
+
+  function parser(payload) {
+    console.log(payload)
+    if (payload.eventID == 'mediaItem.live') {
+      console.log("New live media with id " + payload.payload.id);
     }
-    res.end("Gotcha");
-  });
-
+    if (payload.eventId == 'mediaItem.showOver') {
+      console.log("Show now over, id " + payload.payload.id);
+    }
+    if (payload.eventId == 'mediaItem.notLive') {
+      console.log("Media item listed as not live " + payload.payload.id);
+    }
+    if (payload.eventId == 'mediaItem.vodAvailable') {
+      console.log("New VOD media with id " + payload.payload.id);
+    }
+    if (payload.eventId == 'test') {
+      console.log("Test response with message " + payload.payload.info);
+    }
+  }
 }
 
+webhook.prototype.__proto__ = events.EventEmitter.prototype;
 
-function parser(payload) {
- console.log(payload)
- if (payload.eventID == 'mediaItem.live') {
-    console.log("New live media with id " + payload.payload.id);
-  }
-  if (payload.eventId == 'mediaItem.showOver') {
-    console.log("Show now over, id " + payload.payload.id);
-  }
-  if (payload.eventId == 'mediaItem.notLive') {
-    console.log("Media item listed as not live " + payload.payload.id);
-  }
-  if (payload.eventId == 'mediaItem.vodAvailable') {
-    console.log("New VOD media with id " + payload.payload.id);
-  }
-  if (payload.eventId == 'test') {
-    console.log("Test response with message " + payload.payload.info);
-  }
+exports.create = function(options){
+  return new webhook(options);
 }
