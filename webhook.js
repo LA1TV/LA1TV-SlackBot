d66d3cea7ff9049153d9125f504ccbf766e7ee94
Josh Hodgson <josh.pbh@gmail.com>
2016-04-11 18:35:03 +0100

    New basic web hook parser
    currently logs to console

---
+++ webhook.js
@@ -0,0 +1,33 @@
+var http = require('http');
+var config = require('./config')
+var server = http.createServer(handleRequest);
+server.listen(config.hookport);
+var fullBody=""
+function handleRequest(req, res){
+console.log(req.type)
+  req.on('data', function(chunk) {
+      // append the current chunk of data to the fullBody variable
+      fullBody += chunk.toString();
+    });
+  //  var decodedBody = querystring.parse(fullBody);
+req.on('end', function(){
+    parser(JSON.parse(fullBody));
+  });
+
+  }
+
+
+function parser(payload){
+if(payload.eventID == 'mediaItem.live'){
+  console.log("New live media with id " + payload.payload.id);
+}
+if(payload.eventID == 'mediaItem.showOver'){
+  console.log("Show now over, id " + payload.payload.id);
+}
+if(payload.eventID == 'mediaItem.notLive'){
+  console.log("Media item listed as not live " + payload.payload.id);
+}
+if(payload.eventID == 'mediaItem.vodAvailable'){
+  console.log("New VOD media with id " + payload.payload.id);
+}
+}
