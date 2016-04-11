88681873911485d3e6317ea4053e1c3fd06ff87c
Josh Hodgson <josh.pbh@gmail.com>
2016-04-11 18:38:13 +0100

    send response after request made

--- webhook.js
+++ webhook.js
@@ -1,33 +1,35 @@
 var http = require('http');
-var config = require('./config')
+var config = require('./config');
 var server = http.createServer(handleRequest);
 server.listen(config.hookport);
-var fullBody=""
-function handleRequest(req, res){
-console.log(req.type)
+var fullBody = "";
+
+function handleRequest(req, res) {
+  console.log(req.type);
   req.on('data', function(chunk) {
-      // append the current chunk of data to the fullBody variable
-      fullBody += chunk.toString();
-    });
+    // append the current chunk of data to the fullBody variable
+    fullBody += chunk.toString();
+  });
   //  var decodedBody = querystring.parse(fullBody);
-req.on('end', function(){
+  req.on('end', function() {
     parser(JSON.parse(fullBody));
+    res.send("Gotcha");
   });
 
+}
+
+
+function parser(payload) {
+  if (payload.eventID == 'mediaItem.live') {
+    console.log("New live media with id " + payload.payload.id);
   }
-
-
-function parser(payload){
-if(payload.eventID == 'mediaItem.live'){
-  console.log("New live media with id " + payload.payload.id);
-}
-if(payload.eventID == 'mediaItem.showOver'){
-  console.log("Show now over, id " + payload.payload.id);
-}
-if(payload.eventID == 'mediaItem.notLive'){
-  console.log("Media item listed as not live " + payload.payload.id);
-}
-if(payload.eventID == 'mediaItem.vodAvailable'){
-  console.log("New VOD media with id " + payload.payload.id);
-}
+  if (payload.eventID == 'mediaItem.showOver') {
+    console.log("Show now over, id " + payload.payload.id);
+  }
+  if (payload.eventID == 'mediaItem.notLive') {
+    console.log("Media item listed as not live " + payload.payload.id);
+  }
+  if (payload.eventID == 'mediaItem.vodAvailable') {
+    console.log("New VOD media with id " + payload.payload.id);
+  }
 }
